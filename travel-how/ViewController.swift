8d8421175c260b9cb7ab495148168a4f201c8821
wjdehdry3 <wjdehdry3@gmail.com>
2023-12-12 16:13:28 +0900

    Initial Commit

---
+++ travel-how/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  travel-how
+//
+//  Created by 정동교 on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
